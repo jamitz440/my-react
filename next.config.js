d5b10f7773c0a36b9be3b96f82edd2e28a821e49
jamitz440 <jamster44085@gmail.com>
2023-10-07 13:38:17 +0100

    Full project. working aws as cdn

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: ["d1uoeivxzszfc5.cloudfront.net"],
+  },
+};
